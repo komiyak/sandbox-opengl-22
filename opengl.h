05071a4853b7e067342d3f5294a613d667a8efc3
komiyak <kosuke.komiya@gmail.com>
2022-11-22 16:29:38 +0900

    Delete unused file.

--- opengl.h
+++
@@ -1,9 +0,0 @@
-#ifndef SANDBOX_OPENGL_22_OPENGL_H_
-#define SANDBOX_OPENGL_22_OPENGL_H_
-
-// Use the glad for OpenGL compatibility.
-#include <glad/glad.h>
-// Cross-platform window framework.
-#include <GLFW/glfw3.h>
-
-#endif //SANDBOX_OPENGL_22_OPENGL_H_
